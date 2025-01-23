e1a30fc0c0b000ff776229f567a1af6170e18f98
Lucas Paulo <lp152659@gmail.com>
2025-01-23 19:31:22 -0300

    Mudança nos elementos de landing page

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -8,22 +8,11 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     }
 });
-
 var swiper = new Swiper(".mySwiperLP", {
-    grabCursor: true,
-    effect: "creative",
-    creativeEffect: {
-      prev: {
-        shadow: true,
-        translate: [0, 0, -400],
-      },
-      next: {
-        translate: ["100%", 0, 0],
-      },
-    },
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-    }
-  });
-  
+  effect: "cards",
+  grabCursor: true,
+  autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+  }
+});
