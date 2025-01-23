6dae92b0ec81ace3c932f6761136bde5cad2552f
Lucas Paulo <lp152659@gmail.com>
2025-01-23 00:37:11 -0300

    Mudança de estilo da landing page

---
+++ public/js/reveal.js
@@ -0,0 +1,26 @@
+window.efect = ScrollReveal({reset:true})
+
+efect.reveal('.effectImg', 
+{
+    origin: 'left',
+    duration: 2000,
+    distance: '100px'
+})
+efect.reveal('.effectText', 
+{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '50px'
+})
+efect.reveal('.mySwiperLP', 
+{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '50px'
+})
+efect.reveal('.textDetails', 
+{
+    origin: 'left',
+    duration: 1000,
+    distance: '50px'
+})
